dc4ddbef46f9d4c507ced8b00d1176e4b489c04f
Blue <whatsup@bluepc.me>
2025-09-24 11:51:30 +1200

    added firebase analytics

---
+++ src/routes/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: 'AIzaSyC-vjiFNwNsP8d-tx-KzigMPvUSLt9EPks',
+	authDomain: 'see-word-count.firebaseapp.com',
+	projectId: 'see-word-count',
+	storageBucket: 'see-word-count.firebasestorage.app',
+	messagingSenderId: '734685838265',
+	appId: '1:734685838265:web:b4c3a331acefb43a2b5468',
+	measurementId: 'G-BEDS63TE48'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
