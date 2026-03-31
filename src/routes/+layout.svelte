501ae796049c03f355eacc0d147b8f31ab81990c
Blue <whatsup@bluepc.me>
2026-03-31 13:17:05 +1300

    added about link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,8 +9,11 @@
 <div class="min-h-screen bg-gray-50" lang="en" dir="ltr">
 	<header class="overlay-scrollbar border-b border-gray-300 bg-white">
 		<div class="container mx-auto flex h-16 items-center justify-between px-4">
-			<div class="flex items-center space-x-2">
-				<h1 class="text-xl font-semibold text-gray-900">Word Count Visualizer</h1>
+			<div class="flex w-full items-center justify-between space-x-2">
+				<a href="/">
+					<h1 class="text-xl font-semibold text-gray-900">Word Count Visualizer</h1>
+				</a>
+				<a href="/about" class="text-sm hover:underline">About</a>
 			</div>
 		</div>
 	</header>
