44fe0e5deb2284d34be13ade45a974ea583efe82
Blue <whatsup@bluepc.me>
2026-05-28 20:16:08 +1200

    fix json ld schema

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,18 +3,19 @@
 
 	import './firebase';
 
+	const jsonLd = {
+		'@context': 'https://schema.org',
+		'@type': 'WebSite',
+		name: 'Word Count Visualizer Tool',
+		url: 'https://wordcountvisualizer.com'
+	};
+
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<script type="application/ld+json">
-    {JSON.stringify({
-      "@context": "https://schema.org",
-      "@type": "WebSite",
-      "name": "Word Count Visualizer Tool",
-      "url": "https://wordcountvisualizer.com",
-    })}
-	</script>
+	<!-- eslint-disable-next-line no-useless-escape eslint-disable-next-line svelte/no-at-html-tags -->
+	{@html `<script type="application/ld+json">${JSON.stringify(jsonLd, null, 2)}<\/script>`}
 </svelte:head>
 
 <div class="min-h-screen bg-gray-50" lang="en" dir="ltr">
